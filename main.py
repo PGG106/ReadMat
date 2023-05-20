69bf21ef695d5a63500edcb4145479e1e8cddfb0
PGG106 <g.pagano45@studenti.unisa.it>
2023-05-20 12:08:07 +0200

    Add preprocessing, gpu training

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split
 from torch import nn
 
 import utils
+from preprocess import Preprocess
 
 
 class EegNet(nn.Module):
@@ -29,6 +30,8 @@ class EegNet(nn.Module):
         super().__init__()
         self.linear_layer_stack = nn.Sequential(
             nn.Linear(in_features=input_features, out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units, out_features=hidden_units),
             nn.ReLU(),
             nn.Linear(in_features=hidden_units, out_features=hidden_units),
             nn.ReLU(),
@@ -57,7 +60,7 @@ def get_model_metrics(test_labels, model_predictions):
     cm = confusion_matrix(test_labels, model_predictions)
 
     disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    fig, ax = plt.subplots(figsize=(11, 11))
+    fig, ax = plt.subplots(figsize=(15, 15))
     disp.plot(ax=ax, cmap="binary")
 
     plt.show()
@@ -85,29 +88,42 @@ def RF(train_featuresRF, test_featuresRF, train_labelsRF, test_labelsRF):
     # Create a random forest classifier
     rf = RandomForestClassifier()
     rf.fit(train_featuresRF, train_labelsRF)
+    print("starting RF prediction")
     model_predictions = rf.predict(test_featuresRF)
     get_model_metrics(test_labelsRF, model_predictions)
 
 
-def NN(train_featuresNN, test_featuresNN, train_labelsNN, test_labelsNN):
+def NN(
+        train_featuresNN,
+        test_featuresNN,
+        train_labelsNN,
+        test_labelsNN,
+        input_features):
+    print("Neural network training started")
+
+    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    print(device)
+
     # Create istance of the net
-    model_3 = EegNet(input_features=168,
-                     hidden_units=512,
-                     output_features=21)
+    model_3 = EegNet(input_features=input_features,
+                     hidden_units=768,
+                     output_features=21).to(device)
     # Set loss function and optimizer
     loss_fn = nn.CrossEntropyLoss()
     optimizer = torch.optim.AdamW(model_3.parameters(),
-                                  lr=0.001)
+                                  lr=0.0001)
 
     # Tensor-ise data
     train_features_tensor = torch.tensor(
         train_featuresNN.values).to(
-        torch.float32)
+        torch.float32).to(device)
     test_features_tensor = torch.tensor(
         test_featuresNN.values).to(
-        torch.float32)
-    train_labels_tensor = torch.tensor(train_labelsNN.values)
-    test_labels_tensor = torch.tensor(test_labelsNN.values)
+        torch.float32).to(device)
+    train_labels_tensor = torch.tensor(
+        train_labelsNN.values).to(
+        torch.long).to(device)
+    test_labels_tensor = torch.tensor(test_labelsNN.values).to(torch.long).to(device)
     # Correct for labels 1 indexing
     train_labels_tensor = train_labels_tensor - 1
     test_labels_tensor = test_labels_tensor - 1
@@ -117,10 +133,11 @@ def NN(train_featuresNN, test_featuresNN, train_labelsNN, test_labelsNN):
     # Store loss to plot it
     test_loss_points = []
     train_loss_points = []
+
     # Set a manual seed
     torch.manual_seed(42)
     # Loop through data
-    epochs = 100
+    epochs = 2000
     for epoch in range(epochs):
         # Training
         y_logits = model_3(train_features_tensor)
@@ -133,7 +150,6 @@ def NN(train_featuresNN, test_featuresNN, train_labelsNN, test_labelsNN):
         optimizer.step()
 
         # Testing
-        model_3.eval()
         with torch.inference_mode():
             test_logits = model_3(test_features_tensor)
             test_preds = torch.softmax(test_logits, dim=1).argmax(dim=1)
@@ -145,7 +161,7 @@ def NN(train_featuresNN, test_featuresNN, train_labelsNN, test_labelsNN):
         test_accuracy_points.append(test_acc)
         train_accuracy_points.append(acc)
         test_loss_points.append(test_loss)
-        train_loss_points.append(loss.detach().numpy())
+        train_loss_points.append(loss.detach().cpu().numpy())
 
         if epoch % 10 == 0:
             print(
@@ -157,7 +173,7 @@ def NN(train_featuresNN, test_featuresNN, train_labelsNN, test_labelsNN):
     # adjust for equality between 0 and 1 indexing
     model_predictions += 1
     # Get metrics and confusion matrix for the NN
-    get_model_metrics(test_labelsNN, model_predictions)
+    get_model_metrics(test_labelsNN, model_predictions.cpu())
     # Plot the accuracy
     plt.plot(train_accuracy_points)
     plt.plot(test_accuracy_points)
@@ -187,11 +203,9 @@ def Experiment1(cumulative_df):
     # split in train and test set
     train_features, test_features, train_labels, test_labels = train_test_split(
         features, labels, test_size=0.30)
-    train_labels.sort_values().value_counts(sort=False).plot.bar()
-    # plt.show()
     # RF(train_features, test_features, train_labels, test_labels)
     # SVM(train_features, test_features, train_labels, test_labels)
-    # NN(train_features, test_features, train_labels, test_labels)
+    # NN(train_features, test_features, train_labels, test_labels,168)
 
 
 # Experiment 2: Use an entire session as the test set. This shows the
@@ -209,11 +223,9 @@ def Experiment2(preserve_df):
     train_labels = train['Subject']
     test_features = test.drop('Subject', axis=1)
     test_labels = test['Subject']
-    train_labels.sort_values().value_counts(sort=False).plot.bar()
-    # plt.show()
     # RF(train_features, test_features, train_labels, test_labels)
     # SVM(train_features, test_features, train_labels, test_labels)
-    # NN(train_features, test_features, train_labels, test_labels)
+    # NN(train_features, test_features, train_labels, test_labels,168)
 
 
 # Experiment 3: like Experiment 2, but we use only the file that gave the best result according to the paper.
@@ -231,17 +243,34 @@ def Experiment3():
     train_labels = train['Subject']
     test_features = test.drop('Subject', axis=1)
     test_labels = test['Subject']
+    # RF(train_features, test_features, train_labels, test_labels)
+    # SVM(train_features, test_features, train_labels, test_labels)
+    # NN(train_features, test_features, train_labels, test_labels,168)
+
+
+# Experiment 4: Same as experiment 1 but with an original pre-processed dataset
+def Experiment4():
+    Preprocess()
+    exp4_df = pd.read_csv("Preprocessed_dataset.csv")
+    exp4_df.drop("Session", axis=1, inplace=True)
+    # split again the features and the labels
+    features = exp4_df.drop('Subject', axis=1)
+    labels = exp4_df['Subject']
+    # split in train and test set
+    train_features, test_features, train_labels, test_labels = train_test_split(
+        features, labels, test_size=0.30)
+    print("starting the classification")
     RF(train_features, test_features, train_labels, test_labels)
-    SVM(train_features, test_features, train_labels, test_labels)
-    NN(train_features, test_features, train_labels, test_labels)
+    NN(train_features, test_features, train_labels, test_labels, 11)
 
 
 if __name__ == '__main__':
-    cumulative_df = utils.load_data()
-    # remove columns with null values
-    cumulative_df.dropna(inplace=True)
+    # cumulative_df = utils.load_data()
+    # # remove columns with null values
+    # cumulative_df.dropna(inplace=True)
     # Keep a version of the Dataframe with the Sessions intact for experiment 2
-    preserve_df = cumulative_df.copy()
-    Experiment1(cumulative_df)
-    Experiment2(preserve_df)
-    Experiment3()
+    # preserve_df = cumulative_df.copy()
+    # Experiment1(cumulative_df)
+    # Experiment2(preserve_df)
+    # Experiment3()
+    Experiment4()
---
+++ preprocess.py
@@ -0,0 +1,82 @@
+import os
+
+import imblearn.under_sampling
+import pandas as pd
+from scipy.io import loadmat
+import numpy as np
+from pyprep.prep_pipeline import PrepPipeline
+import matplotlib.pyplot as plt
+import re
+import mne
+from sklearn.decomposition import PCA
+from scipy.signal import butter, filtfilt
+import pywt
+from sklearn.decomposition import PCA
+from imblearn.under_sampling import RandomUnderSampler, EditedNearestNeighbours
+
+
+def get_subject(filename):
+    # initializing substrings
+    sub1 = "s"
+    sub2 = "_"
+    print(filename)
+    # getting index of substrings
+    idx1 = filename.index(sub1) - 1
+    idx2 = filename.index(sub2)
+
+    # length of substring 1 is added to
+    # get string from next character
+    res = filename[idx1 + len(sub1) + 1: idx2]
+    return int(res)
+
+
+def get_session(filename):
+    # initializing substrings
+    sub1 = "_s"
+    sub2 = "."
+
+    # getting index of substrings
+    idx1 = filename.index(sub1) - 1
+    idx2 = filename.index(sub2)
+
+    # length of substring 1 is added to
+    # get string from next character
+    res = filename[idx1 + len(sub1) + 1: idx2]
+    return int(res)
+
+
+def Preprocess():
+    list_of_files = os.listdir("data\\RAW_PARSED\\")
+    cumulative_df = pd.DataFrame()
+    for file in list_of_files:
+        data_set = loadmat("data\\RAW_PARSED\\" + file)['recording']
+        # Transpose to get a signal in each row
+        df = pd.DataFrame(data_set).transpose().copy()
+        # Apply filter
+        b, a = butter(3, [0.1, 0.4], 'band', fs=256.0)
+        signal = filtfilt(b, a, df)
+        coeffs = pywt.wavedec(signal, 'db1', mode='sym', level=2)
+        coeff_df = pd.DataFrame(coeffs[0])
+        coeff_df = coeff_df.transpose()
+        # Reduce the dataset dimensionality
+        pca = PCA(n_components=11)
+        coeff_df = pd.DataFrame(pca.fit_transform(coeff_df))
+        # Add subject and Session labels
+        coeff_df['Subject'] = get_subject(file)
+        coeff_df['Session'] = get_session(file)
+        cumulative_df = pd.concat([cumulative_df, coeff_df])
+
+    # Apply prototype selection
+    print("Starting Undersampling")
+    labels = cumulative_df['Subject']
+    cumulative_df.columns = cumulative_df.columns.astype(str)
+    nm = imblearn.under_sampling.NearMiss()
+    ps_feat, ps_lab = nm.fit_resample(cumulative_df.values, labels)
+    df = pd.DataFrame(ps_feat)
+    columns_names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Subject', 'Session']
+    df.columns = columns_names
+    df = df.iloc[::8]
+    print(df)
+    print("saving preprocessed dataset")
+    df.to_csv("Preprocessed_dataset.csv", index=False)
+    print("Dataset saved")
